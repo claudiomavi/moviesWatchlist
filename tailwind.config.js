d2b3a844933a3e1b6956c691bb9ac8ce8c99eb6c
Claudio <claudio.marino@outlook.com>
2024-10-07 18:23:58 +0200

    ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./public/**/*.{html,js}", "./**/*.{html,js}"],
+  content: [
+    "./public/mywatchlist.html",
+    "./public/mywatchlist.js",
+    "./public/main.js",
+    "./mywatchlist.html",
+    "./mywatchlist.js",
+    "./index.html",
+    "./main.js",
+  ],
   theme: {
     extend: {
       fontFamily: {
